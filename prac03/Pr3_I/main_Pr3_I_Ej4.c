4abeafab0a789062d637046a01230f687fd405e9
Marcelo Suisse <ubriquejazz@gmail.com>
2025-11-29 09:05:29 +0100

    Codigo refactorizado

--- prac03/Pr3_I/main_Pr3_I_Ej4.c
+++ prac03/Pr3_I/main_Pr3_I_Ej4.c
@@ -1,46 +1,32 @@
 #include "delay.h"
+#define USE_MUTEX 1
 
+osMutexId myMutexHandle;
 osSemaphoreId mySemHandle;
 
 int Flag = 1;
 
-void helper(int pin) {
-  osSemaphoreWait(mySemHandle, osWaitForever);
+void helper(int pin, int mutex) {
+  if (mutex) {
+    osMutexWait(myMutexHandle, osWaitForever);
+  }
+  else {
+    osSemaphoreWait(mySemHandle, osWaitForever);
+  }
   if (Flag==1){
-  Flag = 0;
+    Flag = 0;
   } else {
-  HAL_GPIO_WritePin(GPIOD, pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOD, pin, GPIO_PIN_SET);
   }
   //delay_1s();
   delay_ms(50);
+
   HAL_GPIO_WritePin(GPIOD, pin, GPIO_PIN_RESET);
   Flag = 1;
+  if (mutex)
+    osMutexRelease(mySemHandle);
+  else
   osSemaphoreRelease(mySemHandle);
-}
-
-void helper_ia(int pin) {
-    int action_needed = 0; 
-
-    // Acquire Mutex, protect Flag access, then IMMEDIATELY release.
-    osSemaphoreWait(mySemHandle, osWaitForever);
-    if (Flag == 1) {
-        Flag = 0;
-    } else {
-        action_needed = 1; // Signal that a blink is needed
-    }
-    osSemaphoreRelease(mySemHandle);
-
-    // Perform long-duration action OUTSIDE the Mutex protection.
-    if (action_needed == 1) {
-        HAL_GPIO_WritePin(GPIOD, pin, GPIO_PIN_SET);
-        delay_1s(); // Safely blocking/yielding here, Mutex is free
-        HAL_GPIO_WritePin(GPIOD, pin, GPIO_PIN_RESET);
-        
-        // Re-acquire Mutex to safely write to Flag a second time.
-        osSemaphoreWait(mySemHandle, osWaitForever);
-        Flag = 1; // Complete the state change
-        osSemaphoreRelease(mySemHandle);
-    }
 }
 
 /* USER CODE BEGIN Header_StartParpLEDVerde */
@@ -58,7 +44,7 @@ void StartParpLEDVerde(void const * argument)
   for(;;)
   	{
 		HAL_GPIO_WritePin(GPIOD, PIN_GREEN, GPIO_PIN_SET);
-    helper(PIN_BLUE);
+    helper(PIN_BLUE, USE_MUTEX);
 		osDelay(200);
 		HAL_GPIO_WritePin(GPIOD, PIN_GREEN, GPIO_PIN_RESET);
 		osDelay(200);
@@ -81,7 +67,7 @@ void StartParpLEDRojo(void const * argument)
 for(;;)
   {
   HAL_GPIO_WritePin(GPIOD, PIN_RED, GPIO_PIN_SET);
-  helper(PIN_BLUE);
+  helper(PIN_BLUE, USE_MUTEX);
   osDelay(550);
   HAL_GPIO_WritePin(GPIOD, PIN_RED, GPIO_PIN_RESET);
   osDelay(550);
