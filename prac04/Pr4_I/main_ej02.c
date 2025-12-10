e2873cf0ab344499e7890c15cae13dfcccef3010
Marcelo Suisse <ubriquejazz@gmail.com>
2025-12-10 19:35:30 +0100

    Update main_ej02.c

--- prac04/Pr4_I/main_ej02.c
+++ prac04/Pr4_I/main_ej02.c
@@ -8,47 +8,41 @@ void main() {
   MX_GPIO_Init();
 }
 
-// Tarea receptoras
+// Tarea receptora (wait)
 void StartRed(void const * argument)
 {
   for(;;)
   {
     HAL_GPIO_WritePin(GPIOD, PIN_RED, GPIO_PIN_SET);
 		osSignalWait(1, osWaitForever);
-		Blocking_Freq(PIN_RED, 5000, FLASH_HIGH_FREQ);
+		NonBlocking_Freq(PIN_RED, 5000, FLASH_HIGH_FREQ);
   }
 }
 
+// Tarea receptora (wait)
 void StartGreen(void const * argument)
 {
-
-  TickType_t lastWakeTime = xTaskGetTickCount();
   for(;;)
   {
 	  HAL_GPIO_WritePin(GPIOD, PIN_GREEN, GPIO_PIN_SET);
     osSignalWait(0x0001, osWaitForever);
-    Blocking_Freq(PIN_GREEN, 5000, FLASH_HIGH_FREQ);
+    NonBlocking_Freq(PIN_GREEN, 5000, FLASH_HIGH_FREQ);
   }
 }
+
 
 void StartBlue(void const * argument)
 {
   /* USER CODE BEGIN StartRed */
   /* Infinite loop */ 
-  TickType_t lastWakeTime = xTaskGetTickCount();
   for(;;)
   {
-
+    NonBlocking_Freq(PIN_BLUE, 10000, FLASH_LOW_FREQ);
     HAL_GPIO_WritePin(GPIOD, PIN_BLUE, GPIO_PIN_RESET);
-    Blocking_Freq(PIN_BLUE, 10000, FLASH_LOW_FREQ);
 
     osSignalSet(GreenTaskHandle, 1);
-    //Blocking_Freq(PIN_BLUE, 6000, FLASH_HIGH_FREQ);
     HAL_Delay(6000);
-
 		osSignalSet(RedTaskHandle, 1);
-		Blocking_Freq(PIN_BLUE, 6000, FLASH_HIGH_FREQ);
-		//HAL_Delay(6000);
-
+    HAL_Delay(6000);
   }
 }
