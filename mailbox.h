2e2fb6ba7793f977e295389dc7a0e89a7f8ad3f4
Marcelo Suisse <ubriquejazz@gmail.com>
2026-01-04 16:38:49 +0100

    Update mailbox.h

--- mailbox.h
+++ mailbox.h
@@ -10,8 +10,8 @@ typedef struct {
 
 BaseType_t Mailbox_Init(Mailbox_t* handle, int threshold);
 
-BaseType_t Mailbox_Post(Mailbox_t* handle, int new_data); // Tarea remitente (Adquisicion)
+BaseType_t Mailbox_Post(Mailbox_t* handle, int new_data); // Tarea de adquisicion
 
-int Mailbox_Pend(Mailbox_t* handle); // Tarea receptora (Monitoreo)
+int Mailbox_Pend(Mailbox_t* handle); // Tarea de monitoreo (receptora)
 
 #endif
