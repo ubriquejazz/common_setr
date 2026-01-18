f1a317bb4fef72b1c1d5e27755e35668d6e1bdd2
Marcelo Suisse <ubriquejazz@gmail.com>
2026-01-18 14:36:42 +0100

    examples
    FreeRTOS_v1.7 - CMSIS OS API

---
+++ prac05/Pr5_I/main_mutex.c
@@ -0,0 +1,47 @@
+/* Private variables -----------------------------------------*/ 
+osThreadId Task1Handle;
+osThreadId Task2Handle;
+osMutexId myMutex01Handle;
+
+UART_HandleTypeDef huart2;
+
+int _write(int file, char *ptr, int len) {
+	HAL_UART_Transmit(&huart2,(uint8_t *)ptr,len,10);
+	return len;
+}
+
+void main() {
+
+    /* definition and creation of myMutex01 */
+    osMutexDef(myMutex01);
+    myMutex01Handle = osMutexCreate(osMutex(myMutex01));
+
+}
+
+void StartTask1(void const * argument) 
+{
+    /* USER CODE BEGIN StartTask1 */ 
+    /* Infinite loop */
+    for(;;)
+    {
+    osDelay(2000);
+    osMutexWait(myMutex01Handle,1000);
+    printf("Task1 Print\n");
+    osMutexRelease(myMutex01Handle); 
+    }
+    /* USER CODE END 5 */
+}
+
+void StartTask2(void const * argument) 
+{
+    /* USER CODE BEGIN StartTask2 */ 
+    /* Infinite loop */
+    for(;;)
+    {
+    osDelay(2000);
+    osMutexWait(myMutex01Handle,1000);
+    printf("Task2 Print\n");
+    osMutexRelease(myMutex01Handle); 
+    }
+    /* USER CODE END 5 */
+}
---
+++ prac05/Pr5_I/main_queue.c
@@ -0,0 +1,64 @@
+
+
+
+/* Private variables ---------------------------------------------------------*/ 
+osThreadId Sender1Handle;
+osThreadId ReceiverHandle;
+osMessageQId Queue1Handle;
+
+UART_HandleTypeDef huart2;
+
+int _write(int file, char *ptr, int len) {
+	HAL_UART_Transmit(&huart2,(uint8_t *)ptr,len,10);
+	return len;
+}
+
+/* DATA variables ------------------------------------------------------------*/ 
+/* Define the structure type that will be passed on the queue. */ typedef struct
+typedef struct {
+    uint16_t Value;
+    uint8_t Source; 
+} Data;
+
+/* Declare two variables of type Data that will be passed on the queue. */
+Data DataToSend1={0x2018,1};
+Data DataToSend2={0x2019,2};
+
+void main() {
+    /* Create the queue(s) definition and creation of Queue1 */
+    osMessageQDef(Queue1, 256, uint8_t);
+    Queue1Handle = osMessageCreate(osMessageQ(Queue1), NULL);
+}
+
+void StartReceiver(void const * argument)
+{
+    /* USER CODE BEGIN StartReceiver */
+    osEvent retvalue;
+    /* Infinite loop */
+    for(;;) {
+
+        printf("Task2\n");
+        retvalue = osMessageGet(Queue1Handle,4000); 
+
+        if(((Data*)retvalue.value.p)->Source==1){
+            printf("Receiver Receive message from Sender 1\n");
+        }
+        else{
+            printf("Receiver Receive message from Sender 2\n");
+        }
+        printf("%d \n",((Data*)retvalue.value.p)->Value); 
+    }
+    /* USER CODE END StartReceiver */
+}
+
+void StartSender1(void const * argument) 
+{
+    /* USER CODE BEGIN 5 */ 
+    /* Infinite loop */ 
+    for(;;) {
+        printf("Task1\n");
+        osMessagePut(Queue1Handle, (uint32_t)&DataToSend1,200);
+        osDelay(1000);
+    }
+    /* USER CODE END 5 */
+}
